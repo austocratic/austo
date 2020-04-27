7c8b08293d8b7ada51e69f00fd6773885c234d25
Austin Jones <austin@austinsmbp0220.lan>
2020-04-27 14:23:51 -0700

    adding new checklist page. Messing around with html/css

--- routes.js
+++ routes.js
@@ -1,28 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-
-router.get('/lists', (req, res)=>{
-    res.render('pages/lists')
-});
-
 /* GET home page. */
 router.get('/', (req, res)=>{
     res.render('pages/index')
 });
 
-/* OLD template
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  //Template for example code
-  //.set('views', path.join(__dirname, 'views'))
-  //.set('view engine', 'ejs')
-  //.get('/', (req, res) => res.render('pages/index'))
+router.get('/lists', (req, res)=>{
+    res.render('pages/lists')
+});
 
-
-  //.set('view engine', 'html')
-  .get('/', (req, res) => res.send('TEST'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
-
+//Checklist test
+router.get('/checklist', (req, res)=>{
+    res.render('pages/checklist')
+});
 
 module.exports = router;
